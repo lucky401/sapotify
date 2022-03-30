b19e49f8b502111f05defb15565f071a90d4b26c
lucky404 <luckydewa123@gmail.com>
2022-03-30 15:38:03 +0700

    feat: add spotify auth and search track

--- src/components/PlaylistContainer.jsx
+++ src/components/PlaylistContainer.jsx
@@ -1,14 +1,12 @@
 import PlaylistItem from './PlaylistItem';
 
-import { SPOTIFY_PLAYLIST_MOCK_DATA } from '../constants';
-
-function PlaylistContainer() {
+function PlaylistContainer({ tracks }) {
   /**
    * Given a list of Spotify playlist items, return a list of React components that represent each item
    * @returns A list of playlist items.
    */
   function renderPlaylistItems() {
-    return SPOTIFY_PLAYLIST_MOCK_DATA.map((item) => {
+    return tracks.map((item) => {
       const { id, album, name: songName, artists } = item;
       return (
         <PlaylistItem
