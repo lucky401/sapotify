cf24a3e463a4d91aae1df016ea5366e55bf9194a
lucky404 <luckydewa123@gmail.com>
2022-04-07 22:14:58 +0700

    feat: add react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
 import { useEffect, Suspense } from 'react';
 import shallow from 'zustand/shallow';
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  useHistory,
+} from 'react-router-dom';
+
 import { useSelector, useDispatch } from 'react-redux';
 import { setToken } from './store/tokenSlice';
 
@@ -8,13 +15,29 @@ import { useUser } from './store/user';
 
 import { useLogin, useProfile } from './hooks/auth';
 
-import Login from './components/login';
 import Header from './components/header';
 
 // Secret Page
 import PlaylistCreator from './pages/secret/playlist-creator';
 
+import Login from './pages/login';
+
+function SecretPage({ children }) {
+  const history = useHistory();
+  const [getUser] = useUser((state) => [state.getUser], shallow);
+
+  useEffect(() => {
+    const user = getUser();
+    if (!user.token) {
+      history.push('/');
+    }
+  }, []);
+
+  return children;
+}
+
 function App() {
+  const history = useHistory();
   const token = useSelector((state) => state.token.value);
   // eslint-disable-next-line no-console
   console.log('token', token);
@@ -34,19 +57,26 @@ function App() {
     } else {
       dispatch(setToken(user.token));
       getProfile();
+      history.push('/create-playlist');
     }
   }, []);
 
   return (
     <Suspense fallback={<div>Loading</div>}>
       <div className="app">
-        {!logged && <Login />}
-        {logged && (
-          <>
-            <Header />
-            <PlaylistCreator />
-          </>
-        )}
+        {logged && <Header />}
+        <Router>
+          <Switch>
+            <Route path="/">
+              <Login />
+            </Route>
+            <SecretPage>
+              <Route path="/create-playlist">
+                <PlaylistCreator />
+              </Route>
+            </SecretPage>
+          </Switch>
+        </Router>
       </div>
     </Suspense>
   );
